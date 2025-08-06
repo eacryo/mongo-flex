020909da1be46e59c37db71c23345fd412287227
eacryo <eacryo@gmail.com>
2025-08-06 22:46:16 +0800

    Revert "make ReflectUtil only used in this package"
    This reverts commit 9753375ac67ef7099ea008e07a4f2457dc0b2e63.

--- src/main/java/com/github/eacryo/mongoflex/util/ReflectUtil.java
+++ src/main/java/com/github/eacryo/mongoflex/util/ReflectUtil.java
@@ -6,15 +6,14 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
-//没有标public，默认作用域只能在同一个包内访问
-class ReflectUtil {
+public class ReflectUtil {
     /**
      * 获取对象的所有字段（包括继承的字段）
      *
      * @param clazz 目标类
      * @return 字段列表
      */
-    static List<Field> getAllFieldsIncludingInherited(Class<?> clazz) {
+    public static List<Field> getAllFieldsIncludingInherited(Class<?> clazz) {
         List<Field> fields = new ArrayList<>();
 
         // 遍历整个继承层次
