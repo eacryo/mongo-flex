fd281680fd7cc582443bc286513758cfbc437497
eacryo <eacryo@gmail.com>
2025-08-15 23:13:29 +0800

    move getFieldName to ReflectUtil;mapping id to _id

--- src/main/java/com/github/eacryo/mongoflex/util/ReflectUtil.java
+++ src/main/java/com/github/eacryo/mongoflex/util/ReflectUtil.java
@@ -1,9 +1,13 @@
 package com.github.eacryo.mongoflex.util;
 
 import com.github.eacryo.mongoflex.annotation.CollectionId;
+import com.github.eacryo.mongoflex.config.SFunction;
 import org.springframework.util.ReflectionUtils;
+import org.springframework.util.StringUtils;
 
+import java.lang.invoke.SerializedLambda;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.*;
 
 public class ReflectUtil {
@@ -33,11 +37,40 @@ public class ReflectUtil {
 
     public static Field getIdField(List<Field> fields) {
         for (Field field : fields) {
-            if (field.isAnnotationPresent(CollectionId.class)){
+            if (field.isAnnotationPresent(CollectionId.class)) {
                 return field;
             }
         }
         throw new IllegalArgumentException("未找到id对应字段");
+    }
+
+    // 获取方法引用对应的字段名
+    public static <T, R> String getFieldName(SFunction<T, R> func) {
+        try {
+            Method method = func.getClass().getDeclaredMethod("writeReplace");
+            method.setAccessible(true);
+            SerializedLambda lambda = (SerializedLambda) method.invoke(func);
+            String methodName = lambda.getImplMethodName();
+
+            // 解析getter方法名
+            String fieldName;
+            if (methodName.startsWith("get")) {
+                fieldName = StringUtils.uncapitalize(methodName.substring(3));
+            } else if (methodName.startsWith("is")) {
+                fieldName = StringUtils.uncapitalize(methodName.substring(2));
+            } else {
+                fieldName = methodName;
+            }
+            
+            // 特殊处理：id字段映射到MongoDB的_id字段
+            if ("id".equals(fieldName)) {
+                return "_id";
+            }
+            
+            return fieldName;
+        } catch (Exception e) {
+            throw new RuntimeException("解析字段名失败: " + e.getMessage(), e);
+        }
     }
 
 }
