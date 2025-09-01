61ba430037a66d5974fd8ad0b1915598cceb0d7a
eacryo <eacryo@gmail.com>
2025-09-02 07:41:34 +0800

    add remote mongo url

--- src/test/java/com/github/eacryo/mongoflex/testOfEnable/MyTests.java
+++ src/test/java/com/github/eacryo/mongoflex/testOfEnable/MyTests.java
@@ -2,15 +2,19 @@ package com.github.eacryo.mongoflex.testOfEnable;
 
 import com.github.eacryo.mongoflex.bean.Character;
 import com.github.eacryo.mongoflex.bean.GenshinCharacter;
+import com.github.eacryo.mongoflex.config.MongoTemplateFactory;
+import com.github.eacryo.mongoflex.constant.MongoFlexConstant;
 import com.github.eacryo.mongoflex.repository.CharacterRepository;
 import com.github.f4b6a3.ulid.UlidCreator;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Date;
 
-
+@Slf4j
 public class MyTests extends BaseMultiTenantsTest{
 
     @Autowired
@@ -27,6 +31,22 @@ public class MyTests extends BaseMultiTenantsTest{
         character.setName("test");
         character.setBirthday(new Date());
         characterRepository.insert(character);
+    }
+
+    @Test
+    public void testSaveAnotherTenant(){
+        MDC.clear();
+        MDC.put(MongoFlexConstant.TENANT,"remote");
+        Character character = new Character();
+        character.setId(UlidCreator.getUlid().toString());
+        character.setName("Ganyu_"+System.currentTimeMillis());
+        character.setBirthday(new Date());
+        characterRepository.insert(character);
+        Character byId = characterRepository.findById(character.getId());
+        log.info("character:{}",character);
+        log.info("byId:{}",byId);
+        MDC.clear();
+        Assertions.assertEquals(character,byId);
     }
 
     @Test
