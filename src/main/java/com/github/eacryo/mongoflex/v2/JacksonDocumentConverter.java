4b5ac6cd49c80badf65fbbcdb5e7383851b6d458
eacryo <eacryo@gmail.com>
2025-09-28 20:34:57 +0800

    optimize JacksonDocumentConverter

--- src/main/java/com/github/eacryo/mongoflex/v2/JacksonDocumentConverter.java
+++ src/main/java/com/github/eacryo/mongoflex/v2/JacksonDocumentConverter.java
@@ -9,18 +9,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class JacksonDocumentConverter {
 
-    public <T> T convert(Document document,Class<T> clazz) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.addMixIn(clazz, JacksonDocumentMixin.class);
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    private ObjectMapper objectMapper;
+
+    private ObjectMapper getObjectMapper(Class<?> clazz) {
+        if (this.objectMapper == null) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            objectMapper.setAnnotationIntrospector(new FieldMappingIntrospector());
+            this.objectMapper = objectMapper;
+        }
+        this.objectMapper.addMixIn(clazz, JacksonDocumentMixin.class);
+        return objectMapper;
+    }
+
+
+    public <T> T convert(Document document, Class<T> clazz) {
+        ObjectMapper objectMapper = getObjectMapper(clazz);
         return objectMapper.convertValue(document, clazz);
     }
 
     public <T> Document convert(T entity) {
-        //TODO：这里会有两个ObjectMapper实例，考虑优化
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.addMixIn(entity.getClass(), JacksonDocumentMixin.class);
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        ObjectMapper objectMapper = getObjectMapper(entity.getClass());
         try {
             String jsonStr = objectMapper.writeValueAsString(entity);
             return Document.parse(jsonStr);
