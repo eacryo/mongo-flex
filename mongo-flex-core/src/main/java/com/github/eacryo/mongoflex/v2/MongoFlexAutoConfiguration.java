add74f95bb771c31d352369e2b98befe835a8892
eacryo <eacryo@gmail.com>
2026-07-11 10:52:45 +0800

    refactor: rename MyOrmAutoConfiguration to MongoFlexAutoConfiguration

--- mongo-flex-core/src/main/java/com/github/eacryo/mongoflex/v2/MongoFlexAutoConfiguration.java
+++ mongo-flex-core/src/main/java/com/github/eacryo/mongoflex/v2/MongoFlexAutoConfiguration.java
@@ -6,13 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-// @Configuration 告诉 Spring 这是一个配置类
 @Configuration
-// @ConditionalOnClass 确保只有当用户引入了你的 ORM 和 MongoDB 驱动时，这个配置才生效
 @ConditionalOnClass(com.mongodb.client.MongoClient.class)
-// @Import 导入你的注册器，这是核心。它告诉 Spring 在这个配置类加载时，也去执行 MyOrmRegistrar
 @Import(RepositoryRegistrar.class)
-public class MyOrmAutoConfiguration {
+public class MongoFlexAutoConfiguration {
 
     /**
      * 显式注册 MongoMappingConvertor，而非通过 {@code @Component} 扫描。
